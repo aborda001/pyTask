9d6fee9713e9e4ae2b70eba6a73b8541c0d2ff65
Arnaldo Borda <aborda001@gmail.com>
2021-10-25 20:38:18 -0300

    Agregando rutas de logueo

--- index.py
+++ index.py
@@ -1,11 +1,39 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+import sqlite3
 
 app = Flask(__name__)
+baseDeDatos = "database.sqlite"
+
+app.secret_key = "secret_key"
 
 @app.route("/")
 def index():
-	return "<h1>Hola mundo</h1>"
+	return render_template("index.html")
 
+@app.route("/loguear", methods = ['POST'])
+def loguear():
+	username = request.form['username']
+	password = request.form['password']
+
+	if username == usernamedb and password == passworddb:
+		return redirect("/")
+
+	return redirect("/login")
+
+@app.route("/login")
+def login():
+	return render_template("login.html")
+
+@app.before_request
+def middleware():
+	path = request.path
+
+	if path != "/login":
+		return redirect("/login")
+
+@app.errorhandler(404)
+def notFound(e):
+	return render_template("notFound.html")
 
 if __name__ == '__main__':
 	app.run(host= "127.0.0.1",
